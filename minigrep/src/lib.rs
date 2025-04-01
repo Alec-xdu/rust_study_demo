66658c816473a540a324e6cf11a51fa64d2f3444
Alec-xdu <105954365+Alec-xdu@users.noreply.github.com>
2025-04-01 10:11:14 +0800

    1

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,9 @@
+use std::error::Error;
+use std::fs;
+use crate::query::Query;
+
 pub mod query {
-#[derive(Debug)]
+    #[derive(Debug)]
 pub struct Query<'a> {
     keyword: &'a str,
     filename: &'a str,
@@ -17,6 +21,13 @@ impl<'a> Query<'a> {
         })
     }
 
+    pub fn from_string(keyword: &'a str, filename: &'a str) ->  Query<'a> {
+        Query {
+            keyword,
+            filename,
+        }
+    }
+
     pub fn keyword(&self) -> &str {
         self.keyword
     }
@@ -32,4 +43,58 @@ pub fn parse_args(args: &Vec<String>) -> Query {
         filename: args[2].as_str(),
     }
 }
+
+}
+
+pub fn run<'a>(query: &'a Query) -> Result<String, Box<dyn Error>>{
+    let contents = fs::read_to_string(query.filename())?;
+    println!("text:\n{}", contents);
+    Ok(contents)
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Result<Vec<&'a str>, Box<dyn Error>> {
+    let mut vs = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            vs.push(line);
+        }
+    }
+    Ok(vs)
+}
+
+pub fn search_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+#[cfg(test)]
+mod tests {
+    use std::result;
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = Query::from_string("How", "poem.txt");
+        let content = run(&query).unwrap();
+        let result = search(query.keyword() , content.as_str()).unwrap();
+        println!("{:#?}", result);
+    }
+
+    #[test]
+    fn insensitive(){
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."],
+        search_insensitive(query, contents)
+        )
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,19 @@
-use std::env;
+use std::{env, process};
 
 use minigrep::query;
 
 fn main() {
     let v : Vec<String> = env::args().collect();
-    let q = query::Query::new(&v).unwrap();
+    let q = query::Query::new(&v).unwrap_or_else( |err|{
+        println!("problem parsing arguments:{}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {:#?}", q);
+
+    if let Err(e) = minigrep::run(&q) {
+        println!("Application error : {}", e);
+        process::exit(1);
+    }
 
 }
